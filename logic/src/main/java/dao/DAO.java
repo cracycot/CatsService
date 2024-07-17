92017d4dd3a896f5c4a867edd8d834a338ac7edf
Kirill Lesniak <kirill_lesnyak@mail.ru>
2024-07-17 20:47:47 +0300

    TEST made unit tests for DAO

--- logic/src/main/java/dao/DAO.java
+++ logic/src/main/java/dao/DAO.java
@@ -7,7 +7,7 @@ public interface DAO<T> {
     void create(T t) throws SQLException;
     T read(int id) throws SQLException;
 
-    Collection<T> getAll() throws SQLException;
+//    Collection<T> getAll() throws SQLException; ??? скорее всего в service
 
     void remove(int id) throws SQLException;
 }

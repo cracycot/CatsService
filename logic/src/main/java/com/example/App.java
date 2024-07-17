74fa0446df2776e28dd35b9e1f43d06149835be2
Kirill Lesniak <kirill_lesnyak@mail.ru>
2024-07-17 19:18:24 +0300

    FIX structure of project

--- logic/src/main/java/com/example/App.java
+++ logic/src/main/java/com/example/App.java
@@ -1,14 +1,6 @@
 package com.example;
 
-import models.Cat;
-import services.CatService;
-import utils.DataBaseConnection;
-import utils.TruncateTable;
-
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.*;
 
 /**
  * Hello world!
---
+++ logic/src/main/java/utils/TruncateTable.java
@@ -0,0 +1,28 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class TruncateTable extends DataBaseConnection {
+
+
+    public  void truncate(String tableName) throws SQLException {
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = null;
+        String sql = "TRUNCATE TABLE " + tableName;
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+        }
+    }
+}

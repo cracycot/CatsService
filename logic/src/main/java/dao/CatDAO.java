f2f2d2ab3062c2cc1f88dae75f8b3c58c47c1de0
cracycot <kirill_lesnyak@mail.ru>
2024-07-22 20:42:06 +0300

    MADE loger for CatsDAO

--- logic/src/main/java/dao/CatDAO.java
+++ logic/src/main/java/dao/CatDAO.java
@@ -5,24 +5,18 @@ import models.Cat;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import utils.DataBaseConnection;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashMap;
+
 
 public class CatDAO extends DataBaseConnection implements DAO<Cat> {
 
-    private  static  Configuration configuration = new Configuration().addAnnotatedClass(Cat.class).configure();
+    private  static final Configuration configuration = new Configuration().addAnnotatedClass(Cat.class).configure();
+    private static final Logger log = LoggerFactory.getLogger(CatDAO.class);
     private static final SessionFactory sessionFactory = configuration.buildSessionFactory();
 
-
-    public CatDAO() {
-    }
 
     @Override
     public void create(Cat cat) {
@@ -31,15 +25,16 @@ public class CatDAO extends DataBaseConnection implements DAO<Cat> {
             if (cat.getId() == -1) {
                 session.persist(cat);
                 session.getTransaction().commit();
+                log.debug("Cat made {}", cat);
             }
             else {
-                session.update(cat); // надо проверять корректность
+                session.merge(cat);
+                log.debug("Cat update {}", cat);
             }
-//            System.out.println("OK");
-//            System.out.println(cat.getId());
 
         } catch (Exception e) {
-            System.err.println("Ошибка при создании Cat: " + e.getMessage());
+            log.error("Error in made {}", cat);
+            System.err.println("Error in made Cat: " + e.getMessage());
         }
 
     }
@@ -51,11 +46,14 @@ public class CatDAO extends DataBaseConnection implements DAO<Cat> {
             session.beginTransaction();
             cat = session.get(Cat.class, id);
             session.getTransaction().commit();
+            log.debug("Cat was read {}", cat);
         }
         catch (Exception e) {
+            log.error("Cat was not read {}", cat);
             System.err.println("Ошибка при удалении Cat: " + e.getMessage());
         }
         if (cat == null) {
+            log.error("Cat did not found id = {}", id);
             throw new ObjectNotFoundException();
         }
         return cat;
@@ -66,9 +64,11 @@ public class CatDAO extends DataBaseConnection implements DAO<Cat> {
             session.beginTransaction();
             session.merge(cat);
             session.getTransaction().commit();
+            log.debug("Cat was update {}", cat);
         }
         catch (Exception e) {
-            System.err.println("Ошибка при удалении Cat: " + e.getMessage());
+            log.error("Cat did not update {}", cat);
+            System.err.println("Error in update cat" + e.getMessage());
         }
 
     }
@@ -79,203 +79,10 @@ public class CatDAO extends DataBaseConnection implements DAO<Cat> {
             Cat cat = session.get(Cat.class, id);
             session.remove(cat);
             session.getTransaction().commit();
+            log.debug("Cat was remove {}", cat);
         } catch (Exception e) {
+            log.error("Cat was not remove id = {}", id);
             System.err.println("Ошибка при удалении Cat: " + e.getMessage());
         }
     }
-
-//    private void setFriends(int id, HashMap<Integer, Cat> friends, Connection connection) throws SQLException {
-//        String sql = "INSERT INTO catsfriends (id, id1) VALUES (?, ?)";
-//        PreparedStatement preparedStatement  = null;
-//        try {
-//            for (int i : friends.keySet()) {
-//                preparedStatement = connection.prepareStatement(sql);
-//                preparedStatement.setInt(1, id);
-//                preparedStatement.setInt(2, i);
-//                preparedStatement.executeUpdate();
-//            }
-//        }  catch (SQLException e) {
-//            e.printStackTrace();
-//        } // finally вызовется в вызываемом методе
-//
-//    }
-//
-//    private HashMap<Integer, Cat> getFriends(int id, Connection connection) throws  SQLException {
-//        String sql = "SELECT id1 FROM catsfriends WHERE id = ?";
-//        PreparedStatement preparedStatement = null;
-//        HashMap<Integer, Cat> friends = new HashMap<>();
-//        try {
-//            preparedStatement = connection.prepareStatement(sql);
-//            preparedStatement.setInt(1, id);
-//            ResultSet rs = preparedStatement.executeQuery();
-//            while (rs.next()) {
-//                int idFriend = rs.getInt(1);
-//                String sqlForInnerRequest = "SELECT * FROM cats WHERE id = ?";
-//                PreparedStatement preparedStatementForInnerRequest = connection.prepareStatement(sqlForInnerRequest);
-//// Выполнение запроса и работа с результатами
-////                PreparedStatement preparedStatementForInnerRequest = connection.prepareStatement(sqlForInnerRequest);
-//                preparedStatementForInnerRequest.setInt(1, idFriend);
-//                ResultSet friendSet = preparedStatementForInnerRequest.executeQuery();
-//
-//                if (friendSet.next()) {
-//                    String name = friendSet.getString(1);
-//                    LocalDate dateBirth = friendSet.getDate(2).toLocalDate();
-//                    String breed = friendSet.getString(3);
-//                    int idOwner = friendSet.getInt(4);
-//                    Cat friend = new Cat.Builder().name(name)
-//                            .dateBirth(dateBirth)
-//                            .breed(breed)
-//                            .idOwner(idOwner)
-//                            .id(idFriend)
-//                            .build();
-//
-//                    friends.put(idFriend, friend);
-//                }
-//            }
-//        }  catch (SQLException e) {
-//            e.printStackTrace();
-//        } // finally вызовется в вызываемом методе
-//        return friends;
-//
-//    }
-//
-//    private void delFriends(int id, Connection connection) throws SQLException {
-//        String sql = "DELETE FROM catsfriends WHERE id = ?";
-//        PreparedStatement preparedStatement = null;
-//        try {
-//            preparedStatement = connection.prepareStatement(sql);
-//            preparedStatement.setInt(1, id);
-//
-//            preparedStatement.executeUpdate();
-//        }  catch (SQLException e) {
-//            e.printStackTrace();
-//        } // finally вызовется в вызываемом методе
-//    }
-
-//    @Override
-//    public void create(Cat cat) throws SQLException {
-//        Connection connection = getConnection();
-//        PreparedStatement preparedStatement  = null;
-//        String sql = "INSERT INTO cats (name, dateBirth, breed, idOwner, id) VALUES (?, ?, ?, ?, ?)";
-//        try {
-//            preparedStatement = connection.prepareStatement(sql);
-//            preparedStatement.setString(1, cat.getName());
-//            preparedStatement.setDate(2, java.sql.Date.valueOf(cat.getDateBirth()));
-//            preparedStatement.setString(3, cat.getBreed());
-//            preparedStatement.setInt(4, cat.getIdOwner());
-//            preparedStatement.setInt(5, cat.getId());
-//
-//            preparedStatement.executeUpdate();
-//
-//            setFriends(cat.getId(),cat.getFriends(), connection);
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        } finally {
-//            if (preparedStatement != null) {
-//                preparedStatement.close();
-//            }
-//            if (connection != null) {
-//                connection.close();
-//            }
-//        }
-//    }
-
-//    @Override
-//    public Cat read(int id) throws SQLException {
-//        Connection connection = getConnection();
-//        PreparedStatement preparedStatement = null;
-//        String sql = "SELECT * FROM cats WHERE id = ?";
-//        Cat cat = new Cat();
-//        try {
-//            preparedStatement = connection.prepareStatement(sql);
-//            preparedStatement.setInt(1, id);
-//            ResultSet rs = preparedStatement.executeQuery();
-//            if (rs.next()) {
-//                String name = rs.getString(1);
-//                LocalDate dateBirth = rs.getDate(2).toLocalDate();
-//                String breed = rs.getString(3);
-//                int idOwner = rs.getInt(4);
-//                HashMap<Integer, Cat> friends = getFriends(id, connection);
-//                cat = new Cat.Builder().name(name)
-//                        .dateBirth(dateBirth)
-//                        .breed(breed)
-//                        .idOwner(idOwner)
-//                        .id(id)
-//                        .friends(friends)
-//                        .build();
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        } finally {
-//            if (preparedStatement != null) {
-//                preparedStatement.close();
-//            }
-//            if (connection != null) {
-//                connection.close();
-//            }
-//        }
-//        return cat;
-//    }
-
-//    @Override
-//    public ArrayList<Cat> getAll() throws SQLException {
-//        Connection connection = getConnection();
-//        PreparedStatement preparedStatement = null;
-//        String sql = "SELECT * FROM cats";
-//        ArrayList<Cat> cats = new ArrayList<>();
-//        try {
-//            preparedStatement = connection.prepareStatement(sql);
-//            ResultSet rs = preparedStatement.executeQuery();
-//            while (rs.next()) {
-//                String name = rs.getString(1);
-//                LocalDate dateBirth = rs.getDate(2).toLocalDate();
-//                String breed = rs.getString(3);
-//                int idOwner = rs.getInt(4);
-//                int id = rs.getInt(5);
-//                Cat cat = new Cat.Builder().name(name)
-//                        .dateBirth(dateBirth)
-//                        .breed(breed)
-//                        .idOwner(idOwner)
-//                        .id(id)
-//                        .build();
-//                cats.add(cat);
-//            }
-//
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        } finally {
-//            if (preparedStatement != null) {
-//                preparedStatement.close();
-//            }
-//            if (connection != null) {
-//                connection.close();
-//            }
-//        }
-//        return cats;
-//    }
-
-//    @Override
-//    public void remove(int id) throws SQLException {
-//        Connection connection = getConnection();
-//        PreparedStatement preparedStatement = null;
-//        String sql = "DELETE FROM cats WHERE id = ?";
-//        try {
-//            preparedStatement = connection.prepareStatement(sql);
-//            preparedStatement.setInt(1, id);
-//
-//            preparedStatement.executeUpdate();
-//
-//            delFriends(id, connection);
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        } finally {
-//            if (preparedStatement != null) {
-//                preparedStatement.close();
-//            }
-//            if (connection != null) {
-//                connection.close();
-//            }
-//        }
-//    }
 }

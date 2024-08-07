2a8d9debbd8e8a155ee8256b63e0c0c3bd95cae6
cracycot <cracycot@mail.ru>
2024-08-08 00:49:45 +0300

    UPD dto

--- logic/src/main/java/com/exampleLogic/dto/CatDTO.java
+++ logic/src/main/java/com/exampleLogic/dto/CatDTO.java
@@ -1,4 +1,114 @@
 package com.exampleLogic.dto;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 public class CatDTO {
+
+    private String name;
+
+    private LocalDate dateBirth;
+
+    private String breed;
+
+    private Long ownerId;
+
+    private Set<Long> friendsId = new HashSet<>();
+
+    private Long id;
+
+    public static class Builder {
+        private static CatDTO catDTO = new CatDTO();
+
+        public Builder name(String name) {
+            catDTO.name = name;
+            return this;
+        }
+
+        public Builder dateBirth(LocalDate dateBirth) {
+            catDTO.dateBirth = dateBirth;
+            return this;
+        }
+
+        public Builder breed(String breed) {
+            catDTO.breed = breed;
+            return this;
+        }
+
+        public Builder ownerId(Long ownerId) {
+            catDTO.ownerId = ownerId;
+            return this;
+        }
+
+        public Builder friendsId(Set<Long> friendsId) {
+            catDTO.friendsId = friendsId;
+            return this;
+        }
+
+        public Builder id(Long id) {
+            catDTO.id = id;
+            return this;
+        }
+
+        public CatDTO build() {
+            return catDTO;
+        }
+
+
+    }
+
+    public CatDTO() {
+
+   }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDateBirth() {
+        return dateBirth;
+    }
+
+    public void setDateBirth(LocalDate dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public Set<Long> getFriendsId() {
+        return friendsId;
+    }
+
+    public void setFriendsId(Set<Long> friendsId) {
+        this.friendsId = friendsId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+
 }
--- logic/src/main/java/com/exampleLogic/dto/OwnerDTO.java
+++ logic/src/main/java/com/exampleLogic/dto/OwnerDTO.java
@@ -1,4 +1,82 @@
 package com.exampleLogic.dto;
 
-public class OwnerWrapper {
+import com.exampleLogic.models.Cat;
+import com.exampleLogic.models.Owner;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class OwnerDTO {
+    private String name;
+    private LocalDate dateBirth;
+
+    private Long id;
+
+    private Set<Long> catsId = new HashSet<>();
+
+    public static class Builder {
+        private static OwnerDTO ownerDTO = new OwnerDTO(); 
+
+        public OwnerDTO.Builder name(String name) {
+            ownerDTO.name = name;
+            return this;
+        }
+
+        public OwnerDTO.Builder dateBirth(LocalDate dateBirth) {
+            ownerDTO.dateBirth = dateBirth;
+            return this;
+        }
+        public OwnerDTO.Builder catsId(Set<Long> catsId) {
+            ownerDTO.catsId = catsId;
+            return this;
+        }
+
+        public OwnerDTO.Builder id(Long id) {
+            ownerDTO.id = id;
+            return this;
+        }
+
+        public OwnerDTO build() {
+            return ownerDTO;
+        }
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDateBirth() {
+        return dateBirth;
+    }
+
+    public void setDateBirth(LocalDate dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<Long>  getCatsId() {
+        return catsId;
+    }
+
+    public void setCatsId(Set<Long> catsId) {
+        this.catsId = catsId;
+    }
+
 }
